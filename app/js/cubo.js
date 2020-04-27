d6d0a823fabb1033b74f2361cff9b447f199ed4a
Jeison Ortiz <jeisonfernelix@gmail.com>
2020-04-27 12:53:56 -0500

    domEvents implemented :O y funcionó a la primera!

--- app/js/cubo.js
+++ app/js/cubo.js
@@ -2,3 +2,10 @@ var geometry = new THREE.BoxGeometry( 1, 1, 1 );//elemento que contiene vertices
 var material = new THREE.MeshNormalMaterial();//se le añade el material al cubo(en los colores representa hexadecimal)
 var cube = new THREE.Mesh( geometry, material );//objeto que toma una geometría y le aplica un material   y es lo que se transforma
 scene.add( cube );//por defecto se añade la camara(0,0,0)
+
+var domEvents   = new THREEx.DomEvents(camera, renderer.domElement)
+// ...
+domEvents.addEventListener(cube, 'click', function(event){
+    console.log('you clicked on the mesh');
+    rotar_cubo();
+}, false)
