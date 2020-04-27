e3a450e3f722fa22547d082802b9740be538048d
Jeison Ortiz <jeisonfernelix@gmail.com>
2020-04-27 11:28:45 -0500

    100 mm skp collada

--- app/js/axismm.js
+++ app/js/axismm.js
@@ -2,7 +2,7 @@
 
 loader = new THREE.ColladaLoader();
 loader.load('https://triqui.cf/app/assets/axismm.dae', function(collada) {
-  collada.scene.scale.set(1, 1, 1);
+  collada.scene.scale.set(0.95, 0.95, 0.95);
   scene.add(collada.scene);
   return renderer.render(scene, camera);
 });
