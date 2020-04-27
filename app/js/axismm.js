9ce74da227448e45b6bd078072ea4ecdb36b1699
Jeison Ortiz <jeisonfernelix@gmail.com>
2020-04-27 11:19:12 -0500

    prueba con ejes medidos

--- app/js/axismm.js
+++ app/js/axismm.js
@@ -1,7 +1,7 @@
 //Add Board with Collada
 
 loader = new THREE.ColladaLoader();
-loader.load('https://triqui.cf/app/assets/mesas2.dae', function(collada) {
+loader.load('https://triqui.cf/app/assets/axismm.dae', function(collada) {
   collada.scene.scale.set(1, 1, 1);
   scene.add(collada.scene);
   return renderer.render(scene, camera);
