410b5a0990380311f1b0857a56e9ed677ec7c930
Jeison Ortiz <jeisonfernelix@gmail.com>
2020-04-27 06:16:45 -0500

    probando texturas desde el .dae

--- app/js/mesas.js
+++ app/js/mesas.js
@@ -1,7 +1,7 @@
 //Add Board with Collada
 
 loader = new THREE.ColladaLoader();
-loader.load('https://triqui.cf/app/assets/mesas.dae', function(collada) {
+loader.load('https://triqui.cf/app/assets/mesas_texture.dae', function(collada) {
   collada.scene.scale.set(0.01, 0.01, 0.01);
   scene.add(collada.scene);
   return renderer.render(scene, camera);
