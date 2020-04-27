ad5c3fd09eef263c7b18e03f54754c59085bec85
Jeison Ortiz <jeisonfernelix@gmail.com>
2020-04-27 15:53:50 -0500

    refactory de algunas funciones

--- app/js/cubo.js
+++ app/js/cubo.js
@@ -1,11 +1,10 @@
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var geometry = new THREE.BoxGeometry(1,1,1);
 var material = new THREE.MeshNormalMaterial();//se le añade el material al cubo(en los colores representa hexadecimal)
 var cube = new THREE.Mesh( geometry, material );//objeto que toma una geometría y le aplica un material   y es lo que se transforma
 scene.add( cube );
 
-var domEvents   = new THREEx.DomEvents(camera, renderer.domElement)
-// ...
+var domEvents = new THREEx.DomEvents(camera, renderer.domElement);
 domEvents.addEventListener(cube, 'click', function(event){
-    console.log('you clicked on the mesh');
-    rotar_cubo();
-}, false)
+    console.log('click');
+    RotarCubo();
+}, false);
