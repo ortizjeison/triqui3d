c545e1599200105a4f0d8c1ad6568e79809b555a
Jeison Ortiz <jeisonfernelix@gmail.com>
2020-04-27 07:44:09 -0500

    empezando a entender esto de la cámara y el look at

--- app/js/settings.js
+++ app/js/settings.js
@@ -1,6 +1,6 @@
 var scene = new THREE.Scene();//se crea la variable escena en el java
 var aspect = window.innerWidth / window.innerHeight;//variable para la relacion entre alto-ancho
-var camera = new THREE.PerspectiveCamera( 60, aspect, 0.1, 2000 );//set del tamaño (perspectiva) (FOV,aspect.ratio,near,far(todo lo que no se podra ver))
+var camera = new THREE.PerspectiveCamera( 60, aspect, 0.1, 1000 );//set del tamaño (perspectiva) (FOV,aspect.ratio,near,far(todo lo que no se podra ver))
 var renderer = new THREE.WebGLRenderer();
 renderer.setClearColor( 0xffffff );
 renderer.setSize( window.innerWidth, window.innerHeight );//establecer el tamaño en el que queremos que muestre nuestra aplicación
@@ -11,16 +11,16 @@ controls = new THREE.OrbitControls(camera, renderer.domElement);
 
 //Light
 light = new THREE.DirectionalLight(0xffffff, 1.1);
-light.position.set(10, 20, 15);
+light.position.set(0, 20, 0);
 scene.add(light);
 
 //Set Camera position
-camera.position.set(2, 2, 2);
+camera.position.set(0, 5, 0);
 camera.lookAt(new THREE.Vector3(0, 0, 0));
 camera.rotation.z = 5 / 6 * Math.PI;
 
 //Axis
-var axesHelper = new THREE.AxesHelper(2);
+var axesHelper = new THREE.AxesHelper(1);
 scene.add( axesHelper );
 
 //Responsive Function
--- app/js/trans_cubo.js
+++ app/js/trans_cubo.js
@@ -1,6 +1,6 @@
 var rotar_cubo = function () {//bucle que hará que el renderizador dibuje la escena cada vez que se actualice la pantalla 
     requestAnimationFrame( rotar_cubo );//muchas ventajas
-    cube.rotation.x += 0.01;//aplico animacion con la rotacion cada 60/seg
-    cube.rotation.y += 0.01;
+    //cube.rotation.x += 0.01;//aplico animacion con la rotacion cada 60/seg
+    //cube.rotation.y += 0.01;
     renderer.render( scene, camera );
   };
