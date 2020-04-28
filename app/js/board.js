cb86e866682f61d618d286e5ddecf61339c95b40
Jeison Ortiz <jeisonfernelix@gmail.com>
2020-04-27 20:12:24 -0500

    tablero experimental con medidas

--- app/js/board.js
+++ app/js/board.js
@@ -1,7 +1,7 @@
 //Add Board with Collada
 
 loader = new THREE.ColladaLoader();
-loader.load('https://triqui.cf/app/assets/test.dae', function(collada) {
+loader.load('https://triqui.cf/app/assets/board.dae', function(collada) {
   collada.scene.scale.set(collada_ratio, collada_ratio, collada_ratio);
   scene.add(collada.scene);
   return renderer.render(scene, camera);
