1957a5b8a88f2a915a60844b3550e1a660c565cc
Jeison Ortiz <jeisonfernelix@gmail.com>
2020-04-28 08:55:07 -0500

    coordenadas de las posiciones son números enteros

--- app/js/esfera.js
+++ app/js/esfera.js
@@ -1,9 +1,9 @@
-var geometry = new THREE.SphereGeometry(0.43,32,32);
+var geometry = new THREE.SphereGeometry(0.86,32,32);
 var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
 var sphere = new THREE.Mesh( geometry, material );
-sphere.position.x = -1.5;
-sphere.position.z = 1.5;
-sphere.position.y = 0.2;
+sphere.position.x = -3;
+sphere.position.z = 3;
+sphere.position.y = 0.5;
 scene.add( sphere );
 
 
