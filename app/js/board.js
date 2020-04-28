4e2490294df37ad8e3b3518d94069f28c34a19fb
Jeison Ortiz <jeisonfernelix@gmail.com>
2020-04-27 20:17:26 -0500

    no entiendo

--- app/js/board.js
+++ app/js/board.js
@@ -1,5 +1,4 @@
 //Add Board with Collada
-
 loader = new THREE.ColladaLoader();
 loader.load('https://triqui.cf/app/assets/board.dae', function(collada) {
   collada.scene.scale.set(collada_ratio, collada_ratio, collada_ratio);
