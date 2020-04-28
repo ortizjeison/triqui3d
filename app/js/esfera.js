447285f944891c2eda3f71d1ed7c50f01c13af6b
Jeison Ortiz <jeisonfernelix@gmail.com>
2020-04-28 09:44:07 -0500

    cilindro en una posición

--- app/js/esfera.js
+++ app/js/esfera.js
@@ -2,11 +2,18 @@ var geometry = new THREE.SphereGeometry(0.86,32,32);
 var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
 var sphere = new THREE.Mesh( geometry, material );
 sphere.position.x = -3;
-sphere.position.z = 3;
-sphere.position.y = 0.5;
+sphere.position.z = -3;
+sphere.position.y = 0.2;
 scene.add( sphere );
 
-
+var geometry = new THREE.CylinderGeometry(1.1,1.1, 0.45, 32 );
+var material = new THREE.MeshBasicMaterial();
+material.color.set('rgb(255, 127, 64)');
+var cylinder = new THREE.Mesh( geometry, material );
+cylinder.position.x = -3;
+cylinder.position.z = 3;
+cylinder.position.y = 0.2;
+scene.add( cylinder );
 
 
 /*
