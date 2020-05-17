dda481ce79231b01446a7eb59c3aecbabd90c998
Jeison Ortiz <jeisonfernelix@gmail.com>
2020-05-16 19:36:31 -0500

    cambiando a final

--- app/js/board.js
+++ app/js/board.js
@@ -3,7 +3,7 @@ loader = new THREE.ColladaLoader();
 loader.load('https://triqui.cf/app/assets/board.dae', function(collada) {
 
   var la_propia_textura = new THREE.TextureLoader();
-  var textu = la_propia_textura.load( "https://triqui.cf/app/assets/casetas_m.jpg");
+  var textu = la_propia_textura.load( "https://triqui.cf/app/assets/casetas_final.jpg");
   var skyboxGeo = new THREE.SphereGeometry( 400, 400, 400);
   var mat = new THREE.MeshBasicMaterial( { map: textu});  
   var skybox = new THREE.Mesh( skyboxGeo, mat );
